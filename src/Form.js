729b54bc140b9395c21f8b51bb0f967303126c63
barryboubacar <barrybantignel@gmail.com>
2020-07-29 23:00:01 +0200

    composant principal et import du composant Form avec modif

---
+++ src/Form.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './myCss.css'
+
+
+class Form extends Component {
+    render () {
+        return(
+            <div>
+                <h1 className="blue">Commentaire</h1>
+                <button>Valider</button>
+            </div>
+        )
+    }
+}
+export default Form;
